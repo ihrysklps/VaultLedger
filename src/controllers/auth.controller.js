d02b61ff972b7bf6a2664b4cf6b68c5eb96459b0
Hrydyansh Shukla <hrydyanshukla2305@gmail.com>
2026-06-20 01:44:27 +0530

    Initial commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+const userModel = require("../models/usermodel")
+const jwt = require("jsonwebtoken")
+
+async function userRegisteredController(req,res){
+    const{email,password,name} = req.body
+    const isExists = await userModel.findOne({
+        email: email
+    })
+    if(isExists){
+        return res.status(422).json({
+            message:"User already Exists",
+            status: "failed"
+        })
+    }
+    const user = await userModel.create({
+        email,password,name
+    })
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{
+        expiresIn: "3d"})
+        res.cookie("token",token)
+        res.status(201).json({
+            user:{
+                _id:user._id,
+                email:user._email,
+                name:user._name
+            },
+            token
+        })
+}
+async function userLoginController(req,res){
+    const {email,password} = req.body
+    const user = await userModel.findOne({email}).select("+password")
+    if(!user){
+        return res.status(401).json({
+            message: "Email or password is invalid"
+        })
+    }
+    const isValidPassword = await user.comparePassword(password)
+    if(!isValidPassword){
+        return res.status(401).json({
+            message: "Email or password is invalid"
+        })
+    }
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{
+        expiresIn: "3d"})
+        res.cookie("token",token)
+        res.status(200).json({
+            user:{
+                _id:user._id,
+                email:user._email,
+                name:user._name
+            },
+            token
+        })
+}
+module.exports = {
+    userRegisteredController,
+    userLoginController
+}
---
+++ src/models/usermodel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required ofor creating a user"],
+        trim: true,
+        lowecase: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email format"],
+        uniquq: [true, "Entered email already exists"]
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required for creating an account"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required for creation of account"],
+        minlength: [6, "Password should contain more than 6 characters"],
+        select: false
+    }
+}, {
+    timestamps: true
+})
+
+//if a user's password is updated then we will hash the updated password before storing it into the db
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) {
+        return 
+    }
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash
+    return 
+})
+
+userSchema.methods.comparePassword=async function(password){
+    return await bcrypt.compare(password,this.password)
+}   
+
+const userModel = mongoose.model("user",userSchema)
+
+module.exports = userModel

f496c60a6bfb2fe4c88f53006b65484bbd08a352
Hrydyansh Shukla <hrydyanshukla2305@gmail.com>
2026-07-18 01:24:41 +0530

    Rectified database connection failure

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ function connectToDB(){
         console.log("Server is connected to DB")
     })
     .catch(err=>{
-        console.log("Error connecting to DB")
+        console.log("Error connecting to DB",err)
         process.exit(1);
     })
 }
